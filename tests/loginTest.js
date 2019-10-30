1c17db2cfd2445f2877b09480043476e76155201
Gustavo <gustavo.costa@thread.com.br>
2019-10-30 15:38:05 -0300

    configurações e testes iniciais

---
+++ tests/loginTest.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'login com sucesso': function(browser){
+        browser
+        .url('http://zombie-web:5000/login')
+        .waitForElementVisible('.card-login', 2000)
+        .setValue('input[name=email]', 'karol@email.com')
+        .setValue('input[name=password]', "asd")
+        .click('.login-button')
+        .waitForElementVisible('.user .info span', 2000)
+        .assert.containsText('.user .info span', 'karol')
+        .end();
+    }
+}
